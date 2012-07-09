b8644bd7c250e3c30aebde498fcce6e6d8a63cd3
Archimedes Trajano <arch@trajano.net>
2012-07-08 20:38:33 -0400

    Added emerging-technologies branch
    This provides emerging technologies that are being evaluated for
    inclusion into the sactioned technology stack.
    
    This includes the following techonlogies:
    
    * Scala/ScalaTest (for behaviour driven testing)
    * OSGi
    * MongoDB (for NOSQL)
    * Hazelcast for clustering.
    
    This is designed to work with IntelliJ primarily.  However, it is still
    formatted in Eclipse.

---
+++ nosql/src/test/java/net/trajano/nosql/test/MongoDbTest.java
@@ -0,0 +1,97 @@
+package net.trajano.nosql.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.ServerSocket;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+
+import de.flapdoodle.embedmongo.MongoDBRuntime;
+import de.flapdoodle.embedmongo.MongodExecutable;
+import de.flapdoodle.embedmongo.MongodProcess;
+import de.flapdoodle.embedmongo.config.MongodConfig;
+import de.flapdoodle.embedmongo.distribution.Version;
+import de.flapdoodle.embedmongo.runtime.Network;
+
+/**
+ * Shows how to test using MongoDB without CDI. This is useful to show the steps
+ * necessary to use the embedded version of MongoDB for testing. This is based
+ * on http://stackoverflow.com/a/9830861/242042.
+ */
+public class MongoDbTest {
+	private DB db;
+
+	private MongodExecutable mongodExecutable;
+	private MongodProcess mongoProcess;
+
+	@Before
+	public void setUpMongoDB() throws Exception {
+		// Get open port
+		final ServerSocket socket = new ServerSocket(0);
+		final int port = socket.getLocalPort();
+		socket.close();
+
+		// create runtime
+		final MongoDBRuntime runtime = MongoDBRuntime.getDefaultInstance();
+		mongodExecutable = runtime.prepare(new MongodConfig(Version.V2_0_4,
+				port, Network.localhostIsIPv6()));
+		mongoProcess = mongodExecutable.start();
+
+		// create database
+		final Mongo mongo = new Mongo("localhost", port);
+		db = mongo.getDB(UUID.randomUUID().toString());
+	}
+
+	@After
+	public void tearDownMongoDB() throws Exception {
+		// cleanup
+		mongoProcess.stop();
+		mongodExecutable.cleanup();
+	}
+
+	/**
+	 * This is an example based on
+	 * http://www.mongodb.org/display/DOCS/Java+Tutorial to see if things work.
+	 */
+	@Test
+	public void testCreateRuntime() throws Exception {
+
+		// perform operations
+		final DBCollection coll = db.getCollection("testCollection");
+		{
+			final BasicDBObject doc = new BasicDBObject();
+
+			doc.put("name", "MongoDB");
+			doc.put("type", "database");
+			doc.put("count", 1);
+
+			final BasicDBObject info = new BasicDBObject();
+
+			info.put("x", 203);
+			info.put("y", 102);
+
+			doc.put("info", info);
+
+			coll.insert(doc);
+		}
+		{
+			final DBObject myDoc = coll.findOne();
+			assertEquals("MongoDB", myDoc.get("name"));
+			assertEquals(203, ((BasicDBObject) myDoc.get("info")).get("x"));
+		}
+	}
+
+	public void testDatabaseCreated() {
+		assertNotNull(db);
+	}
+}
---
+++ nosql/src/test/java/net/trajano/nosql/test/TestMongoDbProducer.java
@@ -0,0 +1,89 @@
+package net.trajano.nosql.test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.UUID;
+
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.inject.Singleton;
+
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+import de.flapdoodle.embedmongo.MongoDBRuntime;
+import de.flapdoodle.embedmongo.MongodExecutable;
+import de.flapdoodle.embedmongo.MongodProcess;
+import de.flapdoodle.embedmongo.config.MongodConfig;
+import de.flapdoodle.embedmongo.distribution.Version;
+import de.flapdoodle.embedmongo.runtime.Network;
+
+/**
+ * Created with IntelliJ IDEA. User: trajano Date: 12-05-28 Time: 11:05 PM To
+ * change this template use File | Settings | File Templates.
+ */
+public class TestMongoDbProducer {
+	@Produces
+	@Singleton
+	public Mongo createMongo(final MongodProcess process,
+			@EmbeddedMongo final int port) throws Exception {
+		return new Mongo("localhost", port);
+	}
+
+	/**
+	 * Produces a new mongo database using a random name.
+	 * 
+	 * @param mongo
+	 *            mongo instance.
+	 * @return database instance
+	 */
+	@Produces
+	public DB createMongoDB(final Mongo mongo) {
+		return mongo.getDB(UUID.randomUUID().toString());
+	}
+
+	@Produces
+	@Singleton
+	public MongodExecutable createMongodExecutable(@EmbeddedMongo final int port)
+			throws IOException {
+		final MongoDBRuntime runtime = MongoDBRuntime.getDefaultInstance();
+		return runtime.prepare(new MongodConfig(Version.V2_1_1, port, Network
+				.localhostIsIPv6()));
+	}
+
+	@Produces
+	@Singleton
+	public MongodProcess createMongodProcess(
+			final MongodExecutable mongodExecutable) throws IOException {
+		return mongodExecutable.start();
+	}
+
+	public void disposeDB(@Disposes final DB db) {
+		db.dropDatabase();
+	}
+
+	public void disposeMongo(@Disposes final Mongo mongo) {
+		mongo.close();
+	}
+
+	public void disposeMongodExecutable(
+			@Disposes final MongodExecutable mongodExecutable) {
+		mongodExecutable.cleanup();
+	}
+
+	public void disposeMongodProcess(@Disposes final MongodProcess mongodProcess) {
+		System.out.println("stopping process");
+		mongodProcess.stop();
+		System.out.println("stopped process");
+	}
+
+	@Produces
+	@EmbeddedMongo
+	@Singleton
+	public int getOpenPort() throws IOException {
+		final ServerSocket socket = new ServerSocket(0);
+		final int port = socket.getLocalPort();
+		socket.close();
+		return port;
+	}
+}
