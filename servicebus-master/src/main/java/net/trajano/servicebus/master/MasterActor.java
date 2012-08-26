f38ecb2a2faa29826e6cc20ac4223a6cb9b013f8
Archimedes Trajano <arch@trajano.net>
2012-08-26 07:23:20 -0400

    start removing scala

--- servicebus-master/src/main/java/net/trajano/servicebus/master/MasterActor.java
+++ servicebus-master/src/main/java/net/trajano/servicebus/master/MasterActor.java
@@ -1,5 +1,19 @@
 package net.trajano.servicebus.master;
 
-public class MasterActor {
+import akka.actor.UntypedActor;
 
+public class MasterActor extends UntypedActor {
+	// providers.foreach(provider => provider.handles(classOf[_]) &&
+	// provider.newActor(context) ! _)
+	//
+	// private final Set<ActorProvider> providers = new HashSet<>(); def receive
+	// = {
+	// case ActorRegistration(provider) => {
+	// log.info("received test")
+	// System.out.println("!!!!")
+	// providers += provider
+	// }
+	// case _ => {
+	//
+	// }
 }
